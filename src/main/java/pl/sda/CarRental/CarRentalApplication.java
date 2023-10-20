54449d3c3a36048dee2b0265cd3046cf682dd391
jakubkotecki6 <jakubkotecki6@gmail.com>
2023-10-20 13:27:46 +0200

    Initial commit

---
+++ src/main/java/pl/sda/CarRental/CarRentalApplication.java
@@ -0,0 +1,14 @@
+package pl.sda.CarRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class CarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalApplication.class, args);
+	}
+
+}
