5b1c028777809cd0226a50cead2ae67d4ff6587f
Stanislav Latsko <01latsko10@gmail.com>
2023-10-23 19:45:05 +0200

    branch commit #2

--- src/main/java/pl/sda/CarRental/hello/HelloLatskoController.java
+++ src/main/java/pl/sda/CarRental/hello/HelloLatskoController.java
@@ -1,8 +1,15 @@
 package pl.sda.CarRental.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("hello/latsko")
 public class HelloLatskoController {
 
+    @GetMapping
+    public String hello() {
+        return "Hello Stachu";
+    }
 }
