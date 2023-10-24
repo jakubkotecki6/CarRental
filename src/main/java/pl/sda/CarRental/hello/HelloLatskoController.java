0b3427662b88f8f9efb6cde2355c1ffb13eaa572
jakubkotecki6 <jakubkotecki6@gmail.com>
2023-10-24 17:36:20 +0200

    poprawka

--- src/main/java/pl/sda/CarRental/hello/HelloLatskoController.java
+++ src/main/java/pl/sda/CarRental/hello/HelloLatskoController.java
@@ -1,4 +1,4 @@
-package pl.sda.CarRental.hello;
+package pl.sda.carrental.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
