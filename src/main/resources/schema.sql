97820a5578516b3368f0e05abb3b7d5a7165e3b7
Stanislav Latsko <01latsko10@gmail.com>
2023-10-26 17:50:01 +0200

    commit #3 - fixed PostMapping for Employee

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,16 +3,16 @@ DROP TABLE IF EXISTS branch_model;
 
 CREATE TABLE branch_model
 (
-    branch_id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    branch_id BIGINT AUTO_INCREMENT UNIQUE PRIMARY KEY,
     address   VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE employee_model
 (
-    employee_id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    employee_id BIGINT AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
     surname     VARCHAR(255) NOT NULL,
-    position    VARCHAR(255) NOT NULL
+    position    TINYINT NOT NULL
 --     branch_id   INTEGER,
 --     FOREIGN KEY (branch_id) REFERENCES branch_model (branch_id)
 );
