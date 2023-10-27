b3f795241cb21540d0d65b08c13f729631b346cb
jakubkotecki6 <jakubkotecki6@gmail.com>
2023-10-27 19:12:40 +0200

    CarRentalService update

--- src/main/java/pl/sda/carrental/service/CarRentalService.java
+++ src/main/java/pl/sda/carrental/service/CarRentalService.java
@@ -2,6 +2,7 @@ package pl.sda.carrental.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.sda.carrental.ObjectNotFoundInRepositoryException;
 import pl.sda.carrental.model.CarRentalModel;
 import pl.sda.carrental.repository.CarRentalRepository;
 
@@ -11,7 +12,7 @@ public class CarRentalService {
     private final CarRentalRepository carRentalRepository;
 
     public CarRentalModel getCarRental() {
-        return carRentalRepository.findAll().stream().findFirst().orElseThrow(RuntimeException::new);
+        return carRentalRepository.findAll().stream().findFirst().orElseThrow(() -> new ObjectNotFoundInRepositoryException("There is no car rental company"));
     }
 
     public void saveCarRental(CarRentalModel carRental) {
@@ -19,8 +20,16 @@ public class CarRentalService {
     }
 
     public void editCarRental(CarRentalModel carRentalModel) {
+        CarRentalModel edited = carRentalRepository.findAll().stream().findFirst().orElseThrow(() -> new ObjectNotFoundInRepositoryException("There is no car rental company to edit"));
+        edited.setId(carRentalModel.getId());
+        edited.setName(carRentalModel.getName());
+        edited.setAddress(carRentalModel.getAddress());
+        edited.setOwner(carRentalModel.getOwner());
+        edited.setLogo(carRentalModel.getLogo());
+
         carRentalRepository.deleteAll();
-        carRentalRepository.save(carRentalModel);
+
+        carRentalRepository.save(edited);
     }
 
     public void deleteCarRental() {
