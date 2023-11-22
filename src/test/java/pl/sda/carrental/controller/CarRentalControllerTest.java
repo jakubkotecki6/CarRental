fb91f89d15803034401f8c80906a40ffec58a902
jakubkotecki6 <jakubkotecki6@gmail.com>
2023-11-22 17:23:05 +0100

    spring security

--- src/test/java/pl/sda/carrental/controller/CarRentalControllerTest.java
+++ src/test/java/pl/sda/carrental/controller/CarRentalControllerTest.java
@@ -3,6 +3,7 @@ package pl.sda.carrental.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import pl.sda.carrental.model.BranchModel;
 import pl.sda.carrental.model.CarRentalModel;
@@ -11,9 +12,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class CarRentalControllerTest {
+
     @Autowired
     private WebTestClient testClient;
+
     @Test
     void shouldSaveCarRental() {
         Set<BranchModel> branches = new HashSet<>();
