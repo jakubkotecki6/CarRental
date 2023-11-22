7fbfdc25628a3a5b85e536e07589aa05bec032a9
Stanislav Latsko <01latsko10@gmail.com>
2023-11-22 17:45:30 +0100

    commit #16 - added and expanded more security configuration

---
+++ src/main/java/pl/sda/carrental/configuration/TestSecurityConfig.java
@@ -0,0 +1,28 @@
+package pl.sda.carrental.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Profile("test")
+public class TestSecurityConfig {
+    //SecurityFilterChain
+    @Bean
+    public SecurityFilterChain testFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(csrfConfigurer -> csrfConfigurer.disable());
+
+        httpSecurity.authorizeHttpRequests(authorizationMatcher ->
+                authorizationMatcher
+                        .anyRequest()
+                        .permitAll());
+
+        return httpSecurity.build();
+    }
+}
--- src/test/java/pl/sda/carrental/controller/CarRentalControllerTest.java
+++ src/test/java/pl/sda/carrental/controller/CarRentalControllerTest.java
@@ -3,6 +3,7 @@ package pl.sda.carrental.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import pl.sda.carrental.model.BranchModel;
 import pl.sda.carrental.model.CarRentalModel;
@@ -11,6 +12,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class CarRentalControllerTest {
     @Autowired
     private WebTestClient testClient;
@@ -33,6 +35,7 @@ class CarRentalControllerTest {
                 .post()
                 .uri("/carRental")
                 .bodyValue(carRental)
+
                 //when
                 .exchange()
                 //then
