545b9e396b0c751f0d412c5d8ca4558f5a048941
Stanislav Latsko <01latsko10@gmail.com>
2023-12-09 12:45:28 +0100

    commit #23 - finished CRUD for Reservation

--- src/main/java/pl/sda/carrental/controller/ReservationController.java
+++ src/main/java/pl/sda/carrental/controller/ReservationController.java
@@ -2,13 +2,12 @@ package pl.sda.carrental.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.carrental.model.ReservationDTO;
 import pl.sda.carrental.model.ReservationModel;
 import pl.sda.carrental.service.ReservationService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +15,23 @@ import pl.sda.carrental.service.ReservationService;
 public class ReservationController {
     private final ReservationService reservationService;
 
+    @GetMapping
+    public List<ReservationModel> getReservations() {
+        return reservationService.getAllReservations();
+    }
+
     @PostMapping
     public ReservationModel saveReservation(@RequestBody @Valid ReservationDTO reservation) {
         return reservationService.saveReservation(reservation);
+    }
+
+    @PutMapping("/{id}")
+    public ReservationModel editReservation(@PathVariable Long id, @RequestBody ReservationDTO reservationDTO) {
+        return reservationService.editReservation(id, reservationDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReservation(@PathVariable Long id) {
+        reservationService.deleteReservationById(id);
     }
 }
--- src/main/java/pl/sda/carrental/service/ReservationService.java
+++ src/main/java/pl/sda/carrental/service/ReservationService.java
@@ -11,6 +11,7 @@ import pl.sda.carrental.repository.ReservationRepository;
 
 import java.math.BigDecimal;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +23,8 @@ public class ReservationService {
     private final ClientRepository clientRepository;
 
     public ReservationModel saveReservation(ReservationDTO reservationDto) {
+        // cannot create ReservationModel with no parameters!
+        // fields are validated in model as not null
         ReservationModel reservation = new ReservationModel();
 
         setStartEndBranch(reservationDto, reservation);
@@ -47,10 +50,47 @@ public class ReservationService {
     private void setStartEndBranch(ReservationDTO reservationDto, ReservationModel reservation) {
         BranchModel startBranch = branchRepository.findById(reservationDto.startBranchId())
                 .orElseThrow(() -> new ObjectNotFoundInRepositoryException("Branch not found"));
-        reservation.setEndBranch(startBranch);
+        reservation.setStartBranch(startBranch);
         BranchModel endBranch = branchRepository.findById(reservationDto.endBranchId())
                 .orElseThrow(() -> new ObjectNotFoundInRepositoryException("Branch not found"));
         reservation.setEndBranch(endBranch);
     }
 
+    public List<ReservationModel> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+    public ReservationModel editReservation(Long id, ReservationDTO reservationDTO) {
+        ReservationModel foundReservation = reservationRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundInRepositoryException("No reservation under ID #" + id));
+
+        // major part of this code is repeated in the saveReservation() method
+        // have to consider extracting block of code to a separate method
+
+        setStartEndBranch(reservationDTO, foundReservation);
+        foundReservation.setStartDate(reservationDTO.startDate());
+        foundReservation.setEndDate(reservationDTO.endDate());
+
+        CarModel carFromRepo = carRepository.findById(reservationDTO.car_id())
+                .orElseThrow(() -> new ObjectNotFoundInRepositoryException("No car under that ID"));
+        foundReservation.setCar(carFromRepo);
+
+        ClientModel clientFromRepo = clientRepository.findById(reservationDTO.customer_id())
+                .orElseThrow(() -> new ObjectNotFoundInRepositoryException("No customer under that ID"));
+        foundReservation.setCustomer(clientFromRepo);
+
+        long daysDifference = ChronoUnit.DAYS.between(foundReservation.getStartDate(), foundReservation.getEndDate());
+        BigDecimal price = carFromRepo.getPrice().multiply(BigDecimal.valueOf(daysDifference));
+        foundReservation.setPrice(price);
+
+        reservationRepository.deleteById(id);
+        return reservationRepository.save(foundReservation);
+    }
+
+    public void deleteReservationById(Long id) {
+        reservationRepository.findById(id)
+                        .orElseThrow(() ->
+                                new ObjectNotFoundInRepositoryException("No reservation under ID #" + id));
+        reservationRepository.deleteById(id);
+    }
 }
